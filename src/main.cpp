82bf0bd3789dece81e8f4d29f142bf014ddc12dd
zuriBarnes <zurikiser@gmail.com>
2023-03-31 20:43:46 -0400

    add LED animation

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,29 @@
 #include <Arduino.h>
+#define LED_1 12
+#define LED_2 11
+#define LED_3 10
 
 #define MY_INTRO "This app is created in the memory of Travis Barnes"
+int animationSpeed = 0;
 
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
-  Serial.begin(9600);
-  Serial.println(MY_INTRO);
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+  animationSpeed = 90;
+  digitalWrite(LED_1, HIGH);
+  delay(animationSpeed);
+  digitalWrite(LED_1, LOW);
+  delay(animationSpeed);
+  digitalWrite(LED_2, HIGH);
+  delay(animationSpeed);
+  digitalWrite(LED_2, LOW);
+  delay(animationSpeed);
+  digitalWrite(LED_3, HIGH);
+  delay(animationSpeed);
+  digitalWrite(LED_3, LOW);
+  delay(animationSpeed);
 }
