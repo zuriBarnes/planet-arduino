a48a461709d79d63970397517d0668f8a1dee9b3
zuriBarnes <zurikiser@gmail.com>
2023-04-01 14:35:53 -0400

    refactor attempt

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,19 @@
 #define LED_GREEN 11
 #define LED_RED 10
 #define LED_BLUE 9
-#define BRIGHTNESS 75
+#define BRIGHTNESS 255
 #define MY_INTRO "This app is created in the memory of Travis Barnes"
 int animationSpeed = 0;
 
 void setup() {
+  Serial.begin(9600);
   pinMode(LED_BLUE, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
   pinMode(LED_RED, OUTPUT);
 }
 
 void loop() {
-  animationSpeed = 100;
+  animationSpeed = 20;
   
   for(int i = 0; i <= BRIGHTNESS; i++) {
     analogWrite(LED_RED, i);
